97f457cb364cc4998ddd47948d1158f42c0001f1
Minh Hieu Nguyen <minhhi227@gmail.com>
2017-07-01 18:16:13 -0500

    mpp

---
+++ MPPlesson6/src/lesson6/lecture/javafx/prob3/DomException.java
@@ -0,0 +1,13 @@
+package lesson6.lecture.javafx.prob3;
+public class DomException extends Exception {
+	
+	public DomException() {
+		super();
+	}
+	
+	public DomException(String s) {
+		super(s);
+	}
+
+}
+
